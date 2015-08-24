96c0705b38fdfc9a9069c1e7faaa17b83443d7a4
zulus23 <zulus23@gmail.com>
2015-08-24 22:18:40 +0300

    Initial commit

---
+++ app/personalCabApp.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Zhukov on 24.08.2015.
+ */
+
+(function(){
+    angular.module('personalCab',[])
+} )();
---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Zhukov on 24.08.2015.
+ */
+
+
+var gulp = require('gulp');
+
