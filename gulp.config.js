702c4e29a125262a3ed8e69d8b52461c0a8ef44c
zulus23 <zulus23@gmail.com>
2015-08-25 12:54:54 +0300

    setup framework wiredep

---
+++ gulp.config.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Gukov on 25.08.2015.
+ */
+
+
+module.exports = function(){
+     var client = './app/'
+     var config = {
+
+          scssfile: ['content/scss/application.scss'],
+          cssDist: './content/css/',
+          cssFiles:'./content/css/application.css',
+          bowerDir: './bower_components',
+          index: client+'index.html',
+          js: [
+              client+'**/*.module.js',
+               client+'**/*.js',
+          ],
+          client: client,
+          /**
+           * Bower and NPM locations
+           */
+          bower: {
+               json: require('./bower.json'),
+               directory: './bower_components/',
+               ignorePath: '../..'
+          },
+
+     }
+     config.getWiredepDefaultOption = function(){
+          var options = {
+               bowerJson: config.bower.json,
+               directory: config.bower.directory,
+               ignorePath: config.bower.ignorePath
+          };
+          return options;
+
+     };
+
+
+
+     return config;
+}
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,54 @@
  * Created by Zhukov on 24.08.2015.
  */
 
+var gulp    =  require('gulp');
+var config  =  require('./gulp.config')();
+var fs      =  require('fs');
+var del     =  require('del');
+var sass    =  require('gulp-sass');
+var $ = require('gulp-load-plugins')({lazy: true});
 
-var gulp = require('gulp');
+
+gulp.task('compileSass',['cleanCss'],function(){
+    return gulp.src(config.scssfile)
+        .pipe(sass().on('error',sass.logError))
+        .pipe(gulp.dest(config.cssDist));
+});
+
+gulp.task('sassWatch',function(){
+    gulp.watch(config.scssfile,['compileSass'])
+});
+
+gulp.task('cleanCss',function(done){
+    var delconfig = [].concat(config.cssDist);
+    clean(delconfig,done);
+})
+
+gulp.task('wiredep',function(){
+    var options = config.getWiredepDefaultOption();
+    var wiredep =  require('wiredep').stream;
+    return gulp
+        .src(config.index)
+        .pipe(wiredep(options))
+        .pipe($.inject(gulp.src(config.js)))
+        .pipe(gulp.dest(config.client));
+})
+
+gulp.task('inject',['wiredep'],function(){
+
+    return gulp
+        .src(config.index)
+        .pipe($.inject(gulp.src(config.cssFiles)))
+        .pipe(gulp.dest(config.client));
+})
+
+
+
+/* Function */
+
+function clean(path,done){
+   del(path,done);
+}
+
+
 
